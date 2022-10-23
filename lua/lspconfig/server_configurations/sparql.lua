058554e9fdd1b50e61790f9ffa68695cde60f832
Noah Gorstein <ngorstein@gmail.com>
2022-10-23 19:07:47 -0400

    modify sparql server config

--- lua/lspconfig/server_configurations/sparql.lua
+++ lua/lspconfig/server_configurations/sparql.lua
@@ -3,15 +3,11 @@ local util = require 'lspconfig.util'
 local bin_name = 'sparql-language-server'
 local cmd = { 'node', bin_name, '--stdio' }
 
-if vim.fn.has 'win32' == 1 then
-    cmd = { 'cmd.exe', '/C', bin_name, '--stdio' }
-end
-
 return {
     default_config = {
         cmd = cmd,
         filetypes = { 'sparql', 'rq' },
-        root_dir = util.find_git_ancestor,
+        root_dir = util.find_git_ancestor or vim.fn.getcwd(),
         single_file_support = true
     }
 }
